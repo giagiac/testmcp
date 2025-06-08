01d8d12915542343e9fd94b6eaeae5e924aaa373
Giacomo Lavermicocca <giacomo.cervignano@gmail.com>
2025-06-08 16:23:27 +0200

    Primo commit con handler

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to ByteGrad!</h1>
+      <p>
+        Applicazione di e-learning
+      </p>
+    </div>
+  );
+}
